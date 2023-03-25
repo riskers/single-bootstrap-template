8c4f3bec01a7c51a4eb1a7398769f36807bf6a1e
riskers <gaoyibobobo@gmail.com>
2023-03-26 00:29:39 +0800

    feat: Init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+};
